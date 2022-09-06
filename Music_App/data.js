249aadf4738bf8e2bca0f9537b966aa6ea30c329
Abdulrauf <raufisa68@gmail.com>
2022-09-06 11:45:50 +0100

    Updated song images and added music popup list display

--- Music_App/data.js
+++ Music_App/data.js
@@ -2,42 +2,37 @@ const songs = [
   {
     track : "Different World",
     artiste : "Alan Walker",
-    img : "../images/bg6.jpg",
+    img : "../images/music1.jpg",
     src : "../Audios/music1.mp3"
   },
   {
     track : "Shivers",
     artiste : "Ed Sheeran",
-    img : "../images/bg5.jpeg",
+    img : "../images/music2.jpg",
     src : "../Audios/music2.mp3"
   },
   {
     track : "Show Me",
     artiste : "Joeboy",
-    img : "../images/bg4.jpeg",
+    img : "../images/music3.jpg",
     src : "../Audios/music3.mp3"
   },
   {
     track : "All Night Long",
     artiste : "Lionel Richie",
-    img : "../images/bg3.jpeg",
+    img : "../images/music4.jpg",
     src : "../Audios/music4.mp3"
   },
   {
     track : "Find a way to my Heart",
     artiste : "Phil Collins",
-    img : "../images/bg2.jpg",
+    img : "../images/music5.jpg",
     src : "../Audios/music5.mp3"
   },
   {
     track : "Calm Down",
     artiste : "Rema",
-    img : "../images/bg1.jpeg",
+    img : "../images/music6.jpg",
     src : "../Audios/music6.mp3"
   }
 ]
-
-
-
-
-"../images/bg4.jpeg"
--- Music_App/script.js
+++ Music_App/script.js
@@ -1,3 +1,4 @@
+// Selectors
 const main = document.querySelector(".main"),
   musicImg = main.querySelector(".music-img img"),
   musicTitle = main.querySelector(".music-text .title"),
@@ -5,7 +6,7 @@ const main = document.querySelector(".main"),
   musicList = main.querySelector(".music-list"),
   currentAudio = main.querySelector("audio"),
   controls = main.querySelector(".controls"),
-  repeat = controls.querySelector(".fa-repeat"),
+  repeatBtn = controls.querySelector(".fa-repeat"),
   prev = controls.querySelector(".fa-step-backward"),
   next = controls.querySelector(".fa-step-forward"),
   list = controls.querySelector(".fa-list"),
@@ -15,25 +16,29 @@ const main = document.querySelector(".main"),
   progressArea = main.querySelector(".music-progress"),
   progressBar = main.querySelector(".progress-bar"),
   currentTime = main.querySelector(".current"),
-  fullTime = main.querySelector(".end");
+  fullTime = main.querySelector(".end"),
+  musicPopup = main.querySelector(".music-content");
 
-let indexNum = 1;
-
+let musicIndex = 1;
+// Show music popup List
 list.addEventListener("click", () => {
   musicList.classList.add("show");
 });
 cross.addEventListener("click", () => {
   musicList.classList.remove("show");
 });
-const showCurrentSong = () => {
+
+// Load the current song on webpage
+const showCurrentSong = (indexNum) => {
   musicTitle.innerText = songs[indexNum - 1].track;
   musicAuthor.innerText = songs[indexNum - 1].artiste;
   musicImg.src = songs[indexNum - 1].img;
   currentAudio.src = songs[indexNum - 1].src;
   // console.log(currentAudio.src)
 };
-showCurrentSong();
+showCurrentSong(musicIndex);
 
+// Play and Pause song function
 function playSong() {
   main.classList.add("paused");
   playPause.classList = "fa fa-pause-circle play-btn";
@@ -44,41 +49,41 @@ function pauseSong() {
   playPause.classList = "fa fa-play-circle play-btn";
   currentAudio.pause();
 }
-
 playPause.addEventListener("click", () => {
   const isMusicPlaying = main.classList.contains("paused");
   isMusicPlaying ? pauseSong() : playSong();
 });
+
+// Play Next & Previous song Function
 function nextSong() {
-  indexNum++;
-  indexNum > songs.length ? (indexNum = 1) : (indexNum = indexNum);
+  musicIndex++;
+  musicIndex > songs.length ? (musicIndex = 1) : (musicIndex = musicIndex);
 }
 function prevSong() {
-  indexNum--;
-  indexNum < 1 ? (indexNum = songs.length) : (indexNum = indexNum);
+  musicIndex--;
+  musicIndex < 1 ? (musicIndex = songs.length) : (musicIndex = musicIndex);
 }
-
 next.addEventListener("click", () => {
   nextSong();
-  showCurrentSong();
+  showCurrentSong(musicIndex);
   playSong();
 });
 prev.addEventListener("click", () => {
   prevSong();
-  console.log(indexNum);
-  showCurrentSong();
+  showCurrentSong(musicIndex);
   playSong();
 });
 
+// Function to update length of progress bar simultaneously with second timing of current playing song
 currentAudio.addEventListener("timeupdate", (e) => {
+  // Converts the equivalent of the current time to end time to width of progress bar
   let instantTime = e.target.currentTime,
     duration = e.target.duration;
   const newWidth = (instantTime / duration) * 100;
   progressBar.style.width = `${newWidth}%`;
 
+  // Determine the total duration of current song once data is loaded
   currentAudio.addEventListener("loadeddata", () => {
-    // console.log(currentAudio.currentTime, currentAudio.duration)
-
     let fullDuration = currentAudio.duration;
     let timeToMin = Math.floor(fullDuration / 60);
     let timeToSec = Math.floor(fullDuration % 60);
@@ -87,6 +92,7 @@ currentAudio.addEventListener("timeupdate", (e) => {
     }
     fullTime.innerText = `${timeToMin}:${timeToSec}`;
   });
+  // Determine the current duration of song once data is loaded
   let currentDuration = currentAudio.currentTime;
   let timeMin = Math.floor(currentDuration / 60);
   let timeSec = Math.floor(currentDuration % 60);
@@ -96,10 +102,105 @@ currentAudio.addEventListener("timeupdate", (e) => {
   currentTime.innerText = `${timeMin}:${timeSec}`;
 });
 
-progressArea.addEventListener('click', (e) => {
+// Function to add click-drag-audio functionality to progress bar
+progressArea.addEventListener("click", (e) => {
   let progressBarWidth = progressArea.clientWidth;
   let progressOffsetX = e.offsetX;
-  let songDuration = currentAudio.duration
-  currentAudio.currentTime = (progressOffsetX / progressBarWidth) * songDuration;
-  // console.log(progressOffsetX)
-})
+  let songDuration = currentAudio.duration;
+  currentAudio.currentTime =
+    (progressOffsetX / progressBarWidth) * songDuration;
+});
+
+// Repeat, Loop & Shuffle button Function
+repeatBtn.addEventListener("click", () => {
+  let repeatText = repeatBtn.classList[1];
+  switch (repeatText) {
+    case "fa-repeat":
+      repeatBtn.classList.remove("fa-repeat");
+      repeatBtn.classList.add("fa-circle");
+      break;
+    case "fa-circle":
+      repeatBtn.classList.remove("fa-circle");
+      repeatBtn.classList.add("fa-random");
+      break;
+    case "fa-random":
+      repeatBtn.classList.remove("fa-random");
+      repeatBtn.classList.add("fa-repeat");
+      break;
+  }
+});
+
+// Repeat, Loop & Shuffle button image changer function
+currentAudio.addEventListener("ended", () => {
+  let repeatText = repeatBtn.classList[1];
+  switch (repeatText) {
+    case "fa-repeat":
+      nextSong();
+      showCurrentSong(musicIndex);
+      playSong();
+      break;
+    case "fa-circle":
+      currentAudio.currentTime = 0;
+      playSong();
+      break;
+    case "fa-random":
+      let randomIndex = Math.floor(Math.random() * songs.length + 1);
+      // if (!musicIndex == randomIndex) {
+      //   musicIndex = randomIndex
+      //   showCurrentSong(musicIndex)
+      // } else {
+      //   randomIndex = Math.floor((Math.random() * songs.length) +1)
+      // }
+      // showCurrentSong(musicIndex)
+      do {
+        randomIndex = Math.floor(Math.random() * songs.length + 1);
+      } while (musicIndex == randomIndex);
+      musicIndex = randomIndex;
+      showCurrentSong(musicIndex);
+      playSong();
+      break;
+  }
+});
+// for (let i = 0; i < songs.length; i++) {
+//   const musicDiv =
+//   `
+//   <div class="row">
+//     <div class="song">
+//         <p class="song-title">${songs[i].track}</p>
+//         <p>${songs[i].artiste}</p>
+//     </div>
+//     <audio src="${songs[i].src}" ></audio>
+//     <div class="duration"><span>${currentAudio.duration}</span></div>
+//   </div>
+//   `
+
+//   musicPopup.insertAdjacentHTML("beforeend", musicDiv)
+// }
+
+// Function to get full list of songs to display on musicList popup
+songs.map((songs, i) => {
+  const musicDiv = `
+  <div class="row">
+    <div class="song">
+        <p class="song-title">${songs.track}</p>
+        <p>${songs.artiste}</p>
+    </div>
+    <audio src="${songs.src}" id="music-${i}"></audio>
+    <div class="duration-${i}"></div>
+  </div>
+  `;
+  // Function to get full duration of songs to display on musicList popup
+  currentAudio.addEventListener("loadeddata", () => {
+    const listDuration = main.querySelector(`.duration-${i}`);
+    const currentSong = main.querySelector(`#music-${i}`);
+    let fullDuration = currentSong.duration;
+    let timeToMin = Math.floor(fullDuration / 60);
+    let timeToSec = Math.floor(fullDuration % 60);
+    if (timeToSec < 10) {
+      timeToSec = `0${timeToSec}`;
+    }
+
+    listDuration.innerText = `${timeToMin}:${timeToSec}`;
+  });
+  musicPopup.insertAdjacentHTML("beforeend", musicDiv);
+});
